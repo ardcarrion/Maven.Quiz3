3a59d3fd2f680a0cdf4818c250439466061bef11
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-24 17:53:07 -0400

    finished tictactoe

--- src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/TicTacToe.java
@@ -4,34 +4,57 @@ package rocks.zipcode.io.quiz3.arrays;
  * @author leon on 09/12/2018.
  */
 public class TicTacToe {
+    private String[][] board;
 
     public TicTacToe(String[][] board) {
+        this.board = board;
     }
 
     public TicTacToe() {
+        this.board = buildBoard();
+    }
+
+    private String[][] buildBoard() {
+        return new String[][]{{"","",""},{"","",""},{"","",""}};
     }
 
     public String[] getRow(Integer value) {
-        return null;
+        return board[value];
     }
 
     public String[] getColumn(Integer value) {
-        return null;
+        return new String[]{board[0][value], board[1][value], board[2][value]};
     }
 
     public Boolean isRowHomogenous(Integer rowIndex) {
-        return null;
+        String[] row = getRow(rowIndex);
+        return row[0].equals(row[1]) && row[1].equals(row[2]);
     }
 
     public Boolean isColumnHomogeneous(Integer columnIndex) {
-        return null;
+        String[] col = getColumn(columnIndex);
+        return col[0].equals(col[1]) && col[1].equals(col[2]);
+    }
+
+    public Boolean isDiagonalHomogeneous(String[] diagonal) {
+        return diagonal[0].equals(diagonal[1]) && diagonal[1].equals(diagonal[2]);
     }
 
     public String getWinner() {
-        return null;
+        String[] upToDown = {board[0][0], board[1][1], board[2][2]};
+        String[] downToUp = {board[2][0], board[1][1], board[0][2]};
+        if (isDiagonalHomogeneous(upToDown) && !upToDown[0].equals("")) return upToDown[0];
+        if (isDiagonalHomogeneous(downToUp) && !downToUp[0].equals("")) return downToUp[0];
+        for (int i = 0; i < 3; i++) {
+            String[] row = getRow(i);
+            String[] col = getColumn(i);
+            if (isRowHomogenous(i) & !row[0].equals("")) return row[0];
+            if (isColumnHomogeneous(i) & !col[0].equals("")) return col[0];
+        }
+        return "";
     }
 
     public String[][] getBoard() {
-        return null;
+        return this.board;
     }
 }
