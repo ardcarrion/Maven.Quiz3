cb1a10ccc621d018ebdec1a6781890dfa68f1863
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-24 17:35:56 -0400

    finished squarearrayanalyzer

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -1,5 +1,12 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * @author leon on 09/12/2018.
  */
@@ -14,6 +21,17 @@ public class SquareArrayAnalyzer {
      * @return true if each `inputArray[i]` has a corresponding `squaredValues[j]` such that `squaredValues[j] == inputArray[i] * inputArray[i]`
      */
     public static Boolean compare(Integer[] inputArray, Integer[] squaredValues) {
-        return null;
+
+        if (squaredValues.length != inputArray.length) return false;
+        List<Integer> input = Arrays.asList(inputArray);
+        List<Integer> squared = Arrays.asList(squaredValues);
+        Collections.sort(input);
+        Collections.sort(squared);
+        int index = 0;
+        for (Integer i : input) {
+            if (!squared.get(index).equals(i*i)) return false;
+            index++;
+        }
+        return true;
     }
 }
