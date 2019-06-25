7f62d8b69f037b0564e2c86956eb750a0424186c
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-25 08:11:55 -0400

    finished wave

--- src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/WaveGenerator.java
@@ -1,10 +1,21 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class WaveGenerator {
     public static String[] wave(String str) {
-        return null;
+        ArrayList<String> strings = new ArrayList<>();
+        String lowerCase = str.toLowerCase();
+        for (int i = 0; i < lowerCase.length(); i++) {
+            if (Character.isLetter(lowerCase.charAt(i))) {
+                String capital = String.valueOf(lowerCase.charAt(i)).toUpperCase();
+                strings.add(lowerCase.substring(0, i) + capital + lowerCase.substring(i+1, str.length()));
+            }
+        }
+        return strings.toArray(new String[strings.size()]);
     }
 }
