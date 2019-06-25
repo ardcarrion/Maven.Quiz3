fecf62ea5747d96f4304238e5e81ffff36bb4c04
Alicia Carrion <alicia.r.carrion@gmail.com>
2019-06-24 22:47:34 -0400

    37 tests failing

--- src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz3/collections/Student.java
@@ -1,5 +1,7 @@
 package rocks.zipcode.io.quiz3.collections;
 
+import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -7,24 +9,46 @@ import java.util.List;
  */
 public class Student {
 
+    private List<Lab> labs;
     public Student() {
-        this(null);
+        this(new ArrayList<>());
     }
 
     public Student(List<Lab> labs) {
+        this.labs = labs;
     }
 
     public Lab getLab(String labName) {
+        if (labs != null) {
+            for (Lab lab : labs) {
+                if (lab != null && lab.getName().equals(labName)) return lab;
+            }
+        }
         return null;
     }
 
-    public void setLabStatus(String labName, LabStatus labStatus) {
+    public void setLabStatus(String labName, LabStatus labStatus) throws UnsupportedOperationException {
+        if (getLab(labName) == null) throw new UnsupportedOperationException();
+        getLab(labName).setStatus(labStatus);
     }
 
     public void forkLab(Lab lab) {
+        labs.add(lab);
+        lab.setStatus(LabStatus.PENDING);
     }
 
     public LabStatus getLabStatus(String labName) {
-        return null;
+        return getLab(labName).getStatus();
+    }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = labs.size()-1; i >= 0; i--) {
+            Lab lab = labs.get(i);
+            sb.append(lab.getName()).append(" > ")
+                    .append(lab.getStatus().toString()).append("\n");
+        }
+        return sb.toString().trim();
     }
 }
